fb4b9c2553235a26e1c1034f092628406d176c47
GrigoriyKruchinin <gkruchinin75@gmail.com>
2024-05-19 17:31:04 +0200

    Откат collectstatic

--- staticfiles/js/menu.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $('.navbar-toggler').click(function() {
-        $('.navbar-collapse').toggleClass('show');
-    });
-
-    $('.navbar-nav .nav-link').click(function() {
-        $('.navbar-collapse').removeClass('show');
-    });
-});
