f531e3bccae70c1f22784127e5220d69c1ee9e48
GrigoriyKruchinin <gkruchinin75@gmail.com>
2024-05-20 09:06:00 +0200

    Подключил яндекс облако

--- nasa/settings.py
+++ nasa/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "storages",
     "adminsortable2",
     "easy_thumbnails",
     "filer",
@@ -168,11 +169,24 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-# Настройки медиафайлов
-MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Яндекс Облако
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_ENDPOINT_URL = "https://storage.yandexcloud.net"
+# AWS_S3_REGION_NAME = "ru-central1"
+
+# Настройки медиафайлов
+if DEBUG:
+    MEDIA_URL = "/media/"
+else:
+    MEDIA_URL = f"https://{AWS_STORAGE_BUCKET_NAME}.storage.yandexcloud.net/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
