87d6e7d91887b10e50958a88947fe0c9d640e44f
GrigoriyKruchinin <gkruchinin75@gmail.com>
2024-05-22 08:27:26 +0200

    Добавил кадрирование через easy_thumbnails в админке

--- nasa/slider/admin.py
+++ nasa/slider/admin.py
@@ -2,22 +2,24 @@ from django.contrib import admin
 from django.utils.html import format_html
 from adminsortable2.admin import SortableAdminMixin
 from django.utils.translation import gettext_lazy as _
+from easy_thumbnails.files import get_thumbnailer
 from .models import Slide
 
 
 @admin.register(Slide)
 class SlideAdmin(SortableAdminMixin, admin.ModelAdmin):
-    list_display = ("title", "display_image")
+    list_display = ("title", "display_thumbnail")
     ordering = ["order"]
 
-    def display_image(self, obj):
+    def display_thumbnail(self, obj):
         if obj.image:
-            return format_html(
-                '<img src="{}" style="max-width:100px;max-height:100px" />',
-                obj.image.url,
+            thumbnail_options = {"size": (100, 100), "crop": True}
+            thumbnail_url = (
+                get_thumbnailer(obj.image).get_thumbnail(thumbnail_options).url
             )
+            return format_html('<img src="{}" />', thumbnail_url)
         else:
-            return "No image"
+            return _("No image")
 
-    display_image.allow_tags = True
-    display_image.short_description = _("Image")
+    display_thumbnail.allow_tags = True
+    display_thumbnail.short_description = _("Image")
